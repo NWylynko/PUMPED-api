642b2f4d6def1657c4eec6a4c9d16dfe01afa138
Nick Wylynko <nick1014375@gmail.com>
2020-09-05 14:08:40 +0800

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("source-map-support/register");
+console.log('yo');
+//# sourceMappingURL=index.js.map
