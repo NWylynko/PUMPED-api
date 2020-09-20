44a71aa405b5bca258ae476d0e201bb3a547c176
NWylynko <nick1014375@gmail.com>
2020-09-20 06:21:25 +0000

    dont store zsh config in repo

---
+++ .devcontainer/info.sh
@@ -0,0 +1,9 @@
+echo "Helpful Commands: \n" \
+    "yarn start \t | start the built code\n" \
+    "yarn build \t | build the typescript to javascript\n" \
+    "yarn dev \t | run without building ( using ts-node )\n" \
+    "yarn dev:watch  | watch the code and restart on change\n" \
+    "yarn lint \t | point out style issues with the source code\n" \
+    "yarn lint:fix \t | fix styling issues\n" \
+    "yarn test \t | run the tests\n" \
+    "yarn test:watch | re-run the tests on code change\n" 
