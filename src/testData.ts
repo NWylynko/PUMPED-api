f04b18183a536ee787dfdd9d520e44004334c4a3
root <root@ITE23511.ad.ccgs.wa.edu.au>
2020-10-13 12:37:51 +0800

    test data

--- src/testData.ts
+++ src/testData.ts
@@ -1,8 +1,12 @@
-import { addBrand } from "./api/brand"
+import { addBrand } from './api/brand';
+import { addCollection } from './api/collection';
 
 const addTestData = async () => {
-  await addBrand({ name: 'nike', website: 'nike.com' })
-  await addBrand({ name: 'adidas', website: 'nike.com.au' })
-}
+  await addBrand({ name: 'nike', website: 'nike.com' });
+  await addBrand({ name: 'adidas', website: 'nike.com.au' });
 
-export default addTestData
+  await addCollection({ name: 'air max' });
+  await addCollection({ name: 'hypes' });
+};
+
+export default addTestData;
